d4cf884933102a6e8d4178e2b0091f860495506e
MariePorquier <marieporquier@free.fr>
2019-12-02 16:48:14 +0100

    phppartie1exo3

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// déclaration de la variable KM
+$km = 1;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHPpartie1exo3</title>
+  </head>
+  <body>
+      <p><?= $km; ?></p>
+      <p><?= 2 + $km; ?></p>
+      <p><?= -$km * -125; ?></p>
+  </body>
+</html>
